a93acc539b6d123a22219b00cb76069cd587a288
占飞 <2024539581@qq.com>
2018-10-15 00:09:41 +0800

    update

--- src/com/free/Car.java
+++ src/com/free/Car.java
@@ -1,9 +1,19 @@
 package com.free;
 
 public class Car {
-
-	public Car() {
-		// TODO Auto-generated constructor stub
+	int item;
+	String name;
+	double price;
+	int personNum;
+	double tonNum;
+	public Car(int item, String name, double price, int personNum, double tonNum) {
+		super();
+		this.item = item;
+		this.name = name;
+		this.price = price;
+		this.personNum = personNum;
+		this.tonNum = tonNum;
 	}
+	
 
 }
--- src/com/free/Selecter.java
+++ src/com/free/Selecter.java
@@ -2,14 +2,32 @@ package com.free;
 import java.util.Scanner;
 
 public class Selecter {
-	private int nums[];
+	private int[] nums;
+	public int[] getNums() {
+		return nums;
+	}
+
+	public void setNums(int[] nums) {
+		this.nums = nums;
+	}
+
+	public int getDates() {
+		return dates;
+	}
+
+	public void setDates(int dates) {
+		this.dates = dates;
+	}
+
 	private int dates;
 	Scanner input = new Scanner(System.in);
-	public void enter() {
+
+	public void enter(Car c[]) {
 		System.out.println("您是否需要租车？是：1， 否：0");
-		String s1 = input.nextLine();
-		if(s1 == "1") {
+		int s1 = input.nextInt();
+		if(s1 == 1) {
 			System.out.println("欢迎进入达达租车系统");
+			this.printCars(c);
 		} else {
 			System.out.println("期待再次相见");
 		}
@@ -23,13 +41,18 @@ public class Selecter {
 		}
 	}
 	
+	
+	
+	@SuppressWarnings("null")
 	public void printChoice() {
 		System.out.println("您需要租几辆车？");
 		int s = input.nextInt();
+		int[] n = null;
 		for(int i=0;i<s;i++) {
 			System.out.println("请输入第"+(i+1)+"辆车的序号");
-			int n = input.nextInt();
-			this.nums[i] = n;
+			int in = input.nextInt();
+			n[i]=in;
+			this.setNums(n);
 		}
 		System.out.println("请输入你要租的天数");
 		this.dates = input.nextInt();
@@ -44,8 +67,30 @@ public class Selecter {
 		System.out.println("您本次租车总金额为:"+sumPrice*this.dates);
 	}
 	
-	public void printTruck(Car Cars[]) {
+	public void printTruck(Car cars[]) {
 		int sumTon=0;
+		System.out.println("可以载货的汽车有：");
+		for(int i=0;i<this.nums.length;i++) {
+			Car c=cars[this.nums[i]];
+			if(c.tonNum>0) {
+				System.out.print(c.name);
+				sumTon+=c.tonNum;
+			}
+		}
+		System.out.print("总共载货"+sumTon+"吨");
+	}
+	
+	public void printNums(Car cars[]) {
+		int sumPersons=0;
+		System.out.println("可以载客的汽车有：");
+		for(int i=0;i<this.nums.length;i++) {
+			Car c=cars[this.nums[i]];
+			if(c.personNum>0) {
+				System.out.print(c.name);
+				sumPersons+=c.personNum;
+			}
+		}
+		System.out.print("总共载客"+sumPersons+"人");
 	}
 
 }
